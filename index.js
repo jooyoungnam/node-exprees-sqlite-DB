7fd6eb014790daabb9e100a7c56d5ca44650ff05
whitelang <143645324+jooyoungnam@users.noreply.github.com>
2023-09-15 02:52:51 +0900

    first DB CLUD

---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+
+const Comments = sequelize.define('Comments', {
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }, 
+}, {
+});
+
+(async () => { 
+await Comments.sync();
+})();
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.set('view engine', 'ejs');
+
+app.get('/', async function(req, res) {
+  const comments = await Comments.findAll();
+  console.log(comments);
+  res.render('index',{comments : comments }); 
+});
+
+app.post('/create', async function(req, res) {
+    console.log(req.body)
+    const { content } = req.body
+    await Comments.create({ content : content });
+    res.redirect('/')
+});
+
+app.post('/update/:id', async function(req, res) {
+  console.log(req.params)
+  console.log(req.body)
+  const { content } = req.body
+  const { id } = req.params
+  await Comments.update({ content : content }, {
+    where: {
+      id : id
+    }
+  });
+  res.redirect('/');
+});
+
+  app.post('/delete/:id', async function(req, res) {
+    console.log(req.params)
+      const { id } = req.params
+      await Comments.destroy({
+      where: {
+        id : id
+      }
+    });
+   res.redirect('/');
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
